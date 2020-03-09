f3593c8fa5c87db90ee7a9f7637ba3523f06bfc1
João Victor Pereir Santos <joaovictorsantoscpv@gmail.com>
2020-03-09 18:09:30 -0300

    feat: add home module

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quemcontrato_clone/app/utils/constants.dart';
 
 class HomePage extends StatefulWidget {
   final String title;
@@ -12,11 +13,92 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Column(
-        children: <Widget>[],
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Container(
+          child: Column(children: <Widget>[
+            Container(
+              width: MediaQuery.of(context).size.width,
+              margin: EdgeInsets.only(left: 40, top: 60),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    "seja",
+                    style: TextStyle(
+                        color: colorTextOrange,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 80),
+                  ),
+                  Text("bem",
+                      style: TextStyle(
+                          color: colorTextOrange,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 80)),
+                  Text("vindo",
+                      style: TextStyle(
+                          color: colorTextOrange,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 80)),
+                  Text("encontre um profissional ou",
+                      style: TextStyle(color: colorTextOrange, fontSize: 20)),
+                  Text("cadastre seus serviços",
+                      style: TextStyle(color: colorTextOrange, fontSize: 20))
+                ],
+              ),
+            ),
+            Center(
+              child: Column(children: <Widget>[
+                Container(
+                  margin: EdgeInsets.only(top: 20),
+                  decoration: BoxDecoration(
+                    color: colorButtonHome,
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(8.0),
+                        topRight: Radius.circular(8.0)),
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.all(30),
+                    child: Text(
+                      "encontrar profissionais",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 10),
+                  decoration: BoxDecoration(
+                    color: colorButtonHome,
+                    borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(8.0),
+                        bottomRight: Radius.circular(8.0)),
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.all(26),
+                    child: Text(
+                      "cadastrar meus serviços",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 40.0, bottom: 40.0),
+                  child: Text("já sou cadastrado",
+                      style: TextStyle(
+                          color: colorTextOrange,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20)),
+                )
+              ]),
+            )
+          ]),
+        ),
       ),
     );
   }
--- lib/app/utils/constants.dart
+++ lib/app/utils/constants.dart
@@ -1,3 +1,5 @@
 import 'package:flutter/material.dart';
 
 Color colorSplashScreenBackground = Color(0xffFFB800);
+Color colorTextOrange = Color(0xffFC9539);
+Color colorButtonHome = Color(0xffFC9539);
