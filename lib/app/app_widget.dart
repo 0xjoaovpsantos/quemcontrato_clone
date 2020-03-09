0f22234e97fff29da2d68760587441bdbd24447f
João Victor Pereir Santos <joaovictorsantoscpv@gmail.com>
2020-03-09 18:14:54 -0300

    feat: add appbar color

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:quemcontrato_clone/app/utils/constants.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      color: colorAppBar,
       navigatorKey: Modular.navigatorKey,
       title: 'Quem Contrato',
       theme: ThemeData(
--- lib/app/utils/constants.dart
+++ lib/app/utils/constants.dart
@@ -3,3 +3,4 @@ import 'package:flutter/material.dart';
 Color colorSplashScreenBackground = Color(0xffFFB800);
 Color colorTextOrange = Color(0xffFC9539);
 Color colorButtonHome = Color(0xffFC9539);
+Color colorAppBar = Color(0xffFC9539);
